d4b55ac4bd37317e1b4199b8a8f11a7e20f2b8bf
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-24 01:48:27 -0700

    update num2str

--- num2str.c
+++ num2str.c
@@ -9,7 +9,6 @@
 /**
  * num_to_string - creates a string that represents a given integer
  * @num: integer to be represented
- *
  * Return: The representative string
  */
 char *num_to_string(int num)
@@ -19,41 +18,34 @@ char *num_to_string(int num)
 
 	if (num == 0)
 	{
-		str = "0";
-		return (str);
+		return ("0");
 	}
-
 	len = 0;
 	is_negative = 0;
 	if (num < 0)
 	{
 		if (num == INT_MIN)
 		{
-			str = "-2147483648";
 			return (str);
 		}
 		len++;
 		num = 0 - num;
 		is_negative = 1;
 	}
-
 	div = num;
 	do {
 		len++;
 		div /= 10;
 	} while (div > 0);
-
 	str = malloc(sizeof(char) * (len + 1));
 	i = 0;
 	do {
 		str[i++] = (num % 10) + '0';
 		num /= 10;
 	} while (num > 0);
-
 	if (is_negative == 1)
 		str[i++] = '-';
 	str[i] = '\0';
-
 	end = &str[i - 1];
 	start = &str[0];
 	while (start < end)
@@ -62,6 +54,5 @@ char *num_to_string(int num)
 		*(start++) = *end;
 		*(end--) = temp;
 	}
-
 	return (str);
 }
